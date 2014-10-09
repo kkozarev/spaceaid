72a9c293c7ddc651e12d4bac69f301f45050add9
stlstefanov <stl.stefanov@gmail.com>
2014-10-09 12:00:50 +0300

    Tool for converting to JSON
    Simple tool that read shp and converts them to GeoJSON using QGIS'
    ogr2ogr (you'll need to install QGIS from http://www.qgis.org/en/site/
    in order to use it)

---
+++ Tools/Shp2JSON/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Shp2JSON
+{
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = args.Any() ? args.Single() : @"C:\Users\sstefanov\Desktop\Data";
+            var filePaths = Directory.GetFiles(root, "*.shp", SearchOption.AllDirectories);
+            foreach (var path in filePaths)
+            {
+                var outputPath = string.Format(@"{0}\JSON\{1}.geojson", root, Path.GetFileNameWithoutExtension(path));
+                Process.Start(
+                    @"C:\Program Files\QGIS Chugiak\bin\ogr2ogr.exe",
+                    string.Format(" -f GeoJSON -t_srs crs:84 \"{0}\" \"{1}\"", outputPath, path));
+            }
+        }
+    }
+}
