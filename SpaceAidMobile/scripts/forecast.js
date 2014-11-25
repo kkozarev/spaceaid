edcce3daa146cc70bb9104e22659f4701df511fb
Ivan Mladenov <ivanmladenov91@gmail.com>
2014-11-25 09:46:24 +0200

    The app now reads the info from the forecast api, still needs to update the view (html file) with the new info.

--- SpaceAidMobile/scripts/forecast.js
+++ SpaceAidMobile/scripts/forecast.js
@@ -9,12 +9,13 @@
         weatherIcon: "",
         summary: "",
         precipIntensity: 0,
+        precipProbability: 0,
         humidity: 0,
         dewPoint: 0,
         wind: 0,
         pressure: 0,
         hourly: "",
-        weekly: "",
+        daily: "",
     };
     
     window.ForecastViewModel = forecastViewModel;
@@ -33,9 +34,28 @@
         d3.json(url, function(data) {
 		    var dataJson = JSON.parse(data);
             console.log(dataJson);
+            window.ForecastViewModel.temperature = degreesToCelsius(dataJson.currently.temperature);
+            window.ForecastViewModel.temperatureFeel = degreesToCelsius(dataJson.currently.apparentTemperature);
             window.ForecastViewModel.timezone = dataJson.timezone;
-            //etc etc.
+            window.ForecastViewModel.weatherIcon = dataJson.currently.icon;
+            window.ForecastViewModel.summary = dataJson.currently.summary;
+            window.ForecastViewModel.precipIntensity = dataJson.currently.precipIntensity;
+            window.ForecastViewModel.precipProbability = dataJson.currently.precipProbability;
+            window.ForecastViewModel.humidity = dataJson.currently.humidity;
+            window.ForecastViewModel.dewPoint = dataJson.currently.dewPoint;
+            window.ForecastViewModel.wind = dataJson.currently.windSpeed;
+            window.ForecastViewModel.pressure = dataJson.currently.pressure;
+            window.ForecastViewModel.hourly = dataJson.hourly.summary;
+            window.ForecastViewModel.daily = dataJson.daily.summary;
+            if (dataJson.alerts != null) {
+            	window.ForecastViewModel.alerts = dataJson.alerts;
+            }
+            console.log(window.ForecastViewModel);
             //NEEDS TO UPDATE THE DATA MODEL IN HOME.HTML!!!
         });
     };
+    
+    function degreesToCelsius(temp) {
+       return (((parseInt(temp)- 32)*5)/9 ).toFixed(2);
+    }
 })(window);
